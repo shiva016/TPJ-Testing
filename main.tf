5d5061afbdaa71ae870d7ff9743e669dbff0213a
shiva016 <shiva.ku115@gmail.com>
2020-08-30 21:17:45 -0400

    code change

--- main.tf
+++ main.tf
@@ -101,18 +101,18 @@ data "aws_ami" "my_ami" {
  resource "aws_instance" "web-1" {
      ami = "${data.aws_ami.my_ami.id}"
      #ami = "ami-098f16afa9edf40be"
-    #  availability_zone = "us-east-1a"
-    #  instance_type = "t2.micro"
-    #  key_name = "LaptopKey"
-    #  subnet_id = "${aws_subnet.subnet1-public.id}"
-    #  vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-    #  associate_public_ip_address = true	
-    #  tags = {
-    #      Name = "Server-1"
-    #      Env = "Prod"
-    #      Owner = "Sree"
- 	# CostCenter = "ABCD"
-    #  }
+      availability_zone = "us-east-1a"
+      instance_type = "t2.micro"
+     key_name = "LaptopKey"
+     subnet_id = "${aws_subnet.subnet1-public.id}"
+     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+     associate_public_ip_address = true	
+     tags = {
+         Name = "Jenkins-Test"
+         Env = "Prod"
+         Owner = "shiva"
+ 	CostCenter = "ABCD"
+     }
  }
 
 ##output "ami_id" {
