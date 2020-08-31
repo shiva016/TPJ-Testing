720f70fb6d55022fdb36d2500535f2aa70bab543
shiva016 <shiva.ku115@gmail.com>
2020-08-30 22:33:47 -0400

    f

--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@ variable "public_subnet1_name" {}
 variable "public_subnet2_name" {}
 variable "public_subnet3_name" {}
 variable "private_subnet_name" {}
-# variable "imagename" {}
+ variable "imagename" {}
 variable Main_Routing_Table {}
 variable "azs" {
   description = "Run the EC2 Instances in these Availability Zones"
